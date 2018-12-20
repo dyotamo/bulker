757bd24da922c1847b1bd50988bee7efbb4ddb2f
dyotamo <dyotamo@gmail.com>
2018-12-20 11:36:25 +0200

    Adding logic.

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    'DATABASE_URL') or 'sqlite:///shed.db'
+    'DATABASE_URL') or 'sqlite:///sched.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROJECT_ID'] = 'PJ835f870fa34ebe32'
 app.config['SECRET_KEY'] = '8W881_DEsGg3dde8GZLyl2pyMQSQPlDIvJmC'
@@ -23,6 +23,7 @@ db = SQLAlchemy(app)
 
 
 class Contact(db.Model):
+    """ represents the contact entity """
     id = db.Column(db.Integer, primary_key=True)
     contact = db.Column(db.String(13), unique=True, nullable=False)
 
@@ -34,6 +35,7 @@ class Contact(db.Model):
 
 
 class Message(db.Model):
+    """ represents the message to send entity """
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String(1000), unique=False, nullable=False)
 
@@ -44,6 +46,7 @@ class Message(db.Model):
         return '<Message {}>'.format(self.id)
 
 
+# Flask admin interface
 admin = Admin(app, name='Sched', template_mode='bootstrap3')
 admin.add_view(ModelView(Contact, db.session))
 admin.add_view(ModelView(Message, db.session))
@@ -54,10 +57,10 @@ def send_sms():
     import requests
     import json
 
+    from sqlalchemy.sql.expression import func
+
     for contact in Contact.query:
-        query = Message.query
-        index = random.randint(1, query.count())
-        msg = query.get(index)
+        msg = get_random_msg()
 
         headers = {'Content-Type': 'application/json'}
         data = {'content': msg.message, 'to_number': contact.contact}
@@ -85,31 +88,12 @@ def webhook():
     if data.get('event') == 'incoming_message':
         content = data.get('content').upper()
         from_number = data.get('from_number')
-        phone_id = data.get('phone_id')
 
         if content == 'JOIN':
-            # activate
-            contact = Contact(from_number)
-            db.session.add(contact)
-            db.session.commit()
-            return jsonify({
-                'messages': [{
-                    'content':
-                    "Obrigado pela subscrição.\n\nEm breve passará a receber "
-                    "mensagens sobre curiosidades diversas."
-                }]
-            })
-        elif content == 'STOP':
-            # deactivate
-            contact = Contact.query.filter_by(contact=from_number).first()
-            db.session.remove(contact)
-            db.session.commit()
-            return jsonify({
-                'messages': [{
-                    'content':
-                    "A sua subscrição foi cancelada com sucesso.\n\nObrigado."
-                }]
-            })
+            return subscribe(from_number)
+
+        if content == 'STOP':
+            return unsubscribe(from_number)
 
         return jsonify({
             'messages': [{
@@ -120,13 +104,65 @@ def webhook():
         })
 
 
+def get_contact(from_number):
+    """ get the contact object by number """
+    return Contact.query.filter_by(contact=from_number).first()
+
+
+def get_random_msg():
+    """ get random message """
+    return Message.query.order_by(func.random()).first()
+
+
+def subscribe(from_number):
+    """ subscribe contact """
+    if not get_contact(from_number):
+        contact = Contact(from_number)
+        db.session.add(contact)
+        db.session.commit()
+        return jsonify({
+            'messages': [{
+                'content':
+                "Obrigado pela subscrição.\n\nEm breve passará a receber "
+                "mensagens sobre curiosidades diversas."
+            }]
+        })
+    else:
+        return jsonify({
+            'messages': [{
+                'content': "Contacto já subscrito.\n\nObrigado."
+            }]
+        })
+
+
+def unsubscribe(from_number):
+    """ unsubscribe contact """
+    contact = get_contact(from_number)
+    if contact:
+        db.session.remove(contact)
+        db.session.commit()
+        return jsonify({
+            'messages': [{
+                'content':
+                "A sua subscrição foi cancelada com sucesso.\n\nObrigado."
+            }]
+        })
+    else:
+        return jsonify({
+            'messages': [{
+                'content': "Contacto não subscrito.\n\nObrigado."
+            }]
+        })
+
+
+# Web interface errors handlers
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
 
 @app.errorhandler(405)
-def page_not_found(e):
+def method_not_allowed(e):
     return render_template('404.html'), 404
 
 
