2674da507196a7cf05e1bd5af56143d28d03f4ed
dyotamo <dyotamo@gmail.com>
2018-12-20 10:32:36 +0200

    Another chapter added.

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ class Contact(db.Model):
         self.contact = contact
 
     def __repr__(self):
-        return '<Contact %s>' % self.contact
+        return '<Contact {}>'.format(self.contact)
 
 
 class Message(db.Model):
@@ -39,7 +39,7 @@ class Message(db.Model):
         self.message = message
 
     def __repr__(self):
-        return '<Message %s>' % self.id
+        return '<Message {}>'.format(self.id)
 
 
 admin = Admin(app, name='Sched', template_mode='bootstrap3')
@@ -52,51 +52,70 @@ def send_sms():
     import requests
     import json
 
-    query = Message.query
-    index = random.randint(1, query.count())
-    msg = query.get(index)
-
     for contact in Contact.query:
+        query = Message.query
+        index = random.randint(1, query.count())
+        msg = query.get(index)
+
         headers = {'Content-Type': 'application/json'}
         data = {'content': msg.message, 'to_number': contact.contact}
 
         requests.post(
-            'https://api.telerivet.com/v1/projects/%s/messages/send' %
-            app.config['PROJECT_ID'],
+            'https://api.telerivet.com/v1/projects/{}/messages/send'.format(
+                app.config['PROJECT_ID']),
             auth=(app.config['SECRET_KEY'], ''),
             headers=headers,
             data=json.dumps(data))
 
 
 sched = BackgroundScheduler(daemon=True)
-sched.add_job(send_sms, 'interval', minutes=60)
-sched.start()
+sched.add_job(send_sms, 'interval', hours=1)
+# sched.start()
 
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    data = request.data
+    data = request.form
 
     if data.get('secret') != app.config['WEBHOOK_KEY']:
         return jsonify({'error': 'invalid webhook key'}), 403
 
     if data.get('event') == 'incoming_message':
-        content = data.get('content').lower()
+        content = data.get('content').upper()
         from_number = data.get('from_number')
         phone_id = data.get('phone_id')
 
-        if content == 'on':
+        if content == 'JOIN':
             # activate
             contact = Contact(from_number)
             db.session.add(contact)
             db.session.commit()
-        elif content == 'off':
+            return jsonify({
+                'messages': [{
+                    'content':
+                    "Obrigado pela subscrição.\n\nEm breve passará a receber "
+                    "mensagens sobre curiosidades diversas."
+                }]
+            })
+        elif content == 'STOP':
             # deactivate
             contact = Contact.query.filter_by(contact=from_number).first()
             db.session.remove(contact)
             db.session.commit()
+            return jsonify({
+                'messages': [{
+                    'content':
+                    "A sua subscrição foi cancelada com sucesso.\n\nObrigado."
+                }]
+            })
 
-        return jsonify({'messages': [{'content': "Thanks for your message!"}]})
+        return jsonify({
+            'messages': [{
+                'content':
+                "Formato de mensagem inválido.\n\nUse JOIN para ativar "
+                "ou STOP para cancelar a subscrição.\n\nObrigado."
+            }]
+        })
 
 
 @app.errorhandler(404)
